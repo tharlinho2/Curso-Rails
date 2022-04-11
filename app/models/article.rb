09314930c437552498308705ef84bfae69b8d137
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-04-11 09:27:50 -0300

    add styles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, length: {minimum:3, maximum: 60}
-    validates :body, length: {minimum:5, maximum:100}
+    validates :body, length: {minimum:5}
 end
